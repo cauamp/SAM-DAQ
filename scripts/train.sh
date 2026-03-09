bfa67d36a5ccd531e7518c31570ac2b9a88423ff
cauamp <cauamp11@gmail.com>
2026-03-09 15:07:28 -0600

    fix path for sam2 checkpoint in train.sh

--- scripts/train.sh
+++ scripts/train.sh
@@ -12,6 +12,7 @@
 
 CUDA_VISIBLE_DEVICES=0 torchrun --master_port 34563 --nproc_per_node=1 ./train.py \
     --data_prefix /project/def-vislearn/datasets/ \
-    --sam2_config configs/sam2.1/sam2.1_hiera_l.yaml  --sam2_ckpt ./sam2.1_hiera_large.pt \
+    --sam2_config ./configs/sam2.1/sam2.1_hiera_l.yaml \
+    --sam2_ckpt ./models/sam2/sam2.1_hiera_large.pt \
     --task DViSal --frame_length=10 --num_frame_queries=30 --num_video_queries=8 \
     --iters 2000 --batch_size=1 --base_lr=0.0001 --enable_memory # --logpath exps/test
